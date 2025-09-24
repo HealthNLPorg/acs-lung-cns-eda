a014ebe722b06e4d34c9bfbb3207525df6ed5bb0
Eli Goldner <etgld@posteo.us>
2025-09-24 15:07:58 -0700

    Now for additional filters, sampling, and metrics logic

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import os
 import json  # if proves to be too slow or memory consuming look into https://github.com/ICRAR/ijson + https://github.com/lloyd/yajl
 from collections import Counter
 import argparse
+from itertools import chain, groupby
 from functools import reduce, partial, lru_cache
 from collections.abc import Iterable
 from operator import itemgetter
+from typing import Callable, Any
 import datetime
 import logging
 import pathlib
@@ -23,7 +25,7 @@ parser.add_argument(
     "--fields",
     type=str,
     nargs="+",
-    default=["SUBJECT", "PROVIDER_TYPE", "SPECIALTY_NAME"],
+    default=["SUBJECT", "PROVIDER_TYPE", "SPECIALTY_NAME", "PROVIDER_DEPARTMENT"],
     help="Fields for which we want to get the totals",
 )
 
@@ -68,64 +70,29 @@ def mkdir(dir_name: str) -> None:
     _dir_name.mkdir(parents=True, exist_ok=True)
 
 
-def write_unique_keys(
-    output_dir: str, category: str, note_json_list: list[dict[str, str | int]]
-) -> None:
-    with open(os.path.join(output_dir, f"{category}_unique_keys.txt"), mode="w") as f:
-        for key in sorted(
-            set(chain.from_iterable(note_json.keys() for note_json in note_json_list))
-        ):
-            f.write(f"{key}\n")
+def generic_sample(
+    note_json_list: list[dict[str, int | str]],
+    minimum_total_words: int = 500,
+) -> list[dict[str, int | str]]:
+    def has_minimum_total_words(
+        note_json: dict[str, int | str], miminum_total_words: int = minimum_total_words
+    ) -> bool:
+        return note_json.get("RPT_TEXT", "").split() >= miminum_total_words
 
 
-def write_field_totals(
-    output_dir: str, fields: list[str], note_json_list: list[dict[str, str | int]]
-) -> None:
-    def normalize(field: str) -> str:
-        return " ".join(field.split("_")).title()
-
-    field_to_value_totals = {field: Counter() for field in fields}
-    for note_json in note_json_list:
-        for field, counter in field_to_value_totals.items():
-            field_value = note_json.get(field)
-            counter[
-                normalize(field_value)
-                if isinstance(field_value, str)
-                else str(field_value)
-            ] += 1
-    for field, counter in field_to_value_totals.items():
-        pl.DataFrame(
-            sorted(counter.items(), key=itemgetter(1), reverse=True),
-            schema=[normalize(field), "Total"],
-            orient="row",
-        ).write_csv(
-            os.path.join(output_dir, f"{field.lower()}_totals.tsv"), separator="\t"
-        )
+def lmr_sampling(
+    note_json_list: list[dict[str, int | str]], sample_total: int = 250
+) -> list[dict[str, int | str]]:
+    pass
 
 
-def write_totals(output_dir: str, fields: list[str], notes_dir: str) -> None:
-    core_dirname = "_".join(os.path.basename(notes_dir).split()).lower()
-    target_dir = os.path.join(output_dir, f"{core_dirname}_metrics")
-    mkdir(target_dir)
-
-    def get_notes_ls(fn: str) -> list[dict[str, str | int]]:
-        with open(os.path.join(notes_dir, fn)) as f:
-            return json.load(f)["response"]["docs"]
-
-    all_notes_json_ls = list(
-        chain.from_iterable(
-            get_notes_ls(fn) for fn in os.listdir(notes_dir) if fn.endswith("json")
-        )
-    )
-    write_unique_keys(
-        target_dir,
-        core_dirname,
-        all_notes_json_ls,
-    )
-    write_field_totals(target_dir, fields, all_notes_json_ls)
+def inpatient_and_progress_sampling(
+    note_json_list: list[dict[str, int | str]], sample_total: int = 250
+) -> list[dict[str, int | str]]:
+    pass
 
 
-def note_is_qualified(
+def has_valid_mrn_and_date(
     mrn_to_earliest_date: dict[int, str], note_json: dict[str, str | int]
 ) -> bool:
     mrn = int(note_json["DFCI_MRN"])
@@ -134,127 +101,86 @@ def note_is_qualified(
     return is_before(pt_earliest, note_date)
 
 
-# [
-#     "PROVIDER_TYPE",
-#     "EVENT_DATE",
-#     "INP_RPT_TYPE_CD",
-#     "INP_RPT_TYPE",
-#     "PROVIDER_DEPARTMENT",
-#     "RPT_TEXT_NO_HTML",
-#     "RPT_TYPE_STR",
-#     "IMG_EXAM_END_DATE",
-#     "PROVIDER_TYPE_STR",
-#     "PROVIDER_DEPARTMENT_STR",
-#     "LAB_STATUS_CD",
-#     "ENCOUNTER_TYPE_DESC_STR",
-#     "ORD_STATUS_DESC_STR",
-#     "id",
-#     "PROVIDER_CRED",
-#     "DEPT_ID",
-#     "LAST_INDEX_DATE",
-#     "IMPRESSION_TEXT",
-#     "PRACTICE_NAME",
-#     "ENCOUNTER_TYPE_CD",
-#     "PRACTICE_ID",
-#     "RPT_STATUS_STR",
-#     "INSTITUTION_STR",
-#     "AUTHOR_NAME",
-#     "AUTHOR_NAME_STR",
-#     "ORD_STATUS_DESC",
-#     "SUBJECT_STR",
-#     "NARRATIVE_TEXT",
-#     "RPT_DATE",
-#     "RPT_TYPE_CD",
-#     "SOURCE",
-#     "ENCOUNTER_TYPE_DESC",
-#     "DFCI_MRN",
-#     "LAB_STATUS_DESC_STR",
-#     "LAST_UPD_PRACTICE_NAME",
-#     "RPT_TEXT",
-#     "EDW_LAST_MOD_DT",
-#     "AUTHOR_ID",
-#     "EMR_SOURCE",
-#     "SPECIALTY_NAME",
-#     "DATA_SOURCE",
-#     "PROC_DESC_STR",
-#     "RPT_TYPE",
-#     "PRACTICE_NAME_STR",
-#     "SOURCE_STR",
-#     "PROVIDER_CRED_STR",
-#     "LAST_UPD_PRACTICE_ID",
-#     "RPT_ID",
-#     "SPECIALTY_NAME_STR",
-#     "SPEC_TAKEN_DATE",
-#     "ORD_STATUS_CD",
-#     "ENCOUNTER_ID",
-#     "RPT_STATUS_CD",
-#     "PROVIDER_NAME",
-#     "ACCESSION_ID",
-#     "LAST_UPD_PRACTICE_NAME_STR",
-#     "PROC_DESC",
-#     "INSTITUTION",
-#     "RPT_STATUS",
-#     "INP_RPT_TYPE_STR",
-#     "LAB_STATUS_DESC",
-#     "_version_",
-#     "SPECIALTY_ID",
-#     "ADDENDUM_TEXT",
-#     "SUBJECT",
-#     "PROVIDER_NAME_STR",
-#     "PROC_ID",
-# ]
-# CSV rows for reference - just in imaging?
-
-
-def get_qualified_notes_from_csv(
+def get_valid_mrn_and_date_notes_from_csv(
     mrn_to_earliest_date: dict[int, str], csv_path: str
 ) -> list[dict[str, str | int]]:
-    local_qualified = partial(note_is_qualified, mrn_to_earliest_date)
+    local_valid_mrn_and_date = partial(has_valid_mrn_and_date, mrn_to_earliest_date)
     note_json_list = pl.read_csv(csv_path).to_dicts()
-    return [note_json for note_json in note_json_list if local_qualified(note_json)]
+    return [
+        note_json for note_json in note_json_list if local_valid_mrn_and_date(note_json)
+    ]
 
 
-def get_qualified_notes_from_json(
-    mrn_to_earliest_date: dict[int, str], json_path: str
+def get_valid_mrn_and_date_notes_from_json(
+    mrn_to_earliest_date: dict[int, str],
+    json_path: str,
+    debug_source: str | None = None,
 ) -> list[dict[str, str | int]]:
     with open(json_path) as f:
         note_json_list = json.load(f)["response"]["docs"]
-    local_qualified = partial(note_is_qualified, mrn_to_earliest_date)
-    return [note_json for note_json in note_json_list if local_qualified(note_json)]
+    local_valid_mrn_and_date = partial(has_valid_mrn_and_date, mrn_to_earliest_date)
+    results = [
+        note_json for note_json in note_json_list if local_valid_mrn_and_date(note_json)
+    ]
+    if debug_source is not None:
+        for node_json in results:
+            node_json["debug_source"] = debug_source
+    return results
 
 
-def get_dir_totals(
+def get_dir_to_valid_mrn_and_date_notes(
     mrn_to_earliest_date: dict[int, str], notes_dir: str
-) -> Iterable[Counter[int]]:
-    for root, dirs, files in os.walk(notes_dir):
-        root_total = 0
-        for fn in files:
-            if fn.lower().endswith("json"):
-                file_counter = get_file_totals_json(
-                    mrn_to_earliest_date, os.path.join(root, fn)
-                )
-                yield file_counter
-                root_total += sum(file_counter.values())
-            if fn.lower().endswith("csv"):
-                file_counter = get_file_totals_csv(
-                    mrn_to_earliest_date, os.path.join(root, fn)
-                )
-                yield file_counter
-                root_total += sum(file_counter.values())
-        if root_total > 0:
-            logger.info(
-                f"Total qualifying files in {os.path.basename(root)}: {root_total}"
+) -> dict[str, list[dict[str, str | int]]]:
+    def get_valid_mrn_and_date_notes(root: str, fn: str) -> list[dict[str, str | int]]:
+        if fn.lower().endswith("json"):
+            return get_valid_mrn_and_date_notes_from_json(
+                mrn_to_earliest_date, os.path.join(root, fn)
             )
+        elif fn.lower().endswith("csv"):
+            return get_valid_mrn_and_date_notes_from_csv(
+                mrn_to_earliest_date, os.path.join(root, fn)
+            )
+        else:
+            ValueError(f"{os.path.join(root, fn)} has bad extension")
+            return []
+
+    return {
+        os.path.basename(root): get_valid_mrn_and_date_notes(root, fn, root)
+        for root, dirs, files in os.walk(notes_dir)
+        for fn in files
+    }
 
 
-def get_totals(
-    mrn_to_earliest_date: dict[int, str], notes_dir: str
-) -> Iterable[tuple[int, int]]:
-    full_counter = reduce(
-        Counter.__add__, get_dir_totals(mrn_to_earliest_date, notes_dir)
-    )
-    # return sorted by totals
-    return sorted(full_counter.items(), key=itemgetter(1), reverse=True)
+# NB: depending on the predicates used, there may be notes/folders left out,
+# this is intentional for adjusting later
+def merge_by_named_predicates(
+    dir_to_valid_mrn_and_date_notes: dict[str, list[dict[str, str | int]]],
+    name_to_predicate: dict[str, Callable[[str], bool]],
+) -> dict[str, list[dict[str, str | int]]]:
+    def grouped_by_predicate(
+        predicate: Callable[[str], bool],
+    ) -> Iterable[str, list[dict[str, str | int]]]:
+        def key_fn(dir_and_notes: tuple[str, list[dict[str, str | int]]]) -> bool:
+            dirname, _ = dir_and_notes
+            return predicate(dirname)
+
+        return groupby(
+            sorted(dir_to_valid_mrn_and_date_notes.items(), key=key_fn), key=key_fn
+        )
+
+    def re_grouped_notes(
+        predicate: Callable[[str], bool],
+    ) -> list[dict[str, str | int]]:
+        for is_group, note_clusters in grouped_by_predicate(predicate):
+            if is_group:
+                return list(chain.from_iterable(note_clusters))
+        ValueError("Something wrong with clustering - check in more detail")
+        return []
+
+    return {
+        predicate_name: re_grouped_notes(predicate)
+        for predicate_name, predicate in name_to_predicate.items()
+    }
 
 
 def collect_notes_and_write_metrics(
@@ -275,13 +201,28 @@ def collect_notes_and_write_metrics(
             mrn_and_date_df["mrn"], mrn_and_date_df["earliest_date"]
         )
     }
-
-    final_sorted_df = pl.DataFrame(
-        get_totals(mrn_to_earliest_date, notes_dir),
-        schema=["MRN", "TOTAL_AFTER_EARLIEST"],
-        orient="row",
+    dir_to_valid_mrn_and_date_notes = get_dir_to_valid_mrn_and_date_notes(
+        mrn_to_earliest_date, notes_dir
     )
-    final_sorted_df.write_csv(os.path.join(output_dir, "totals.csv"), separator=",")
+
+    def is_one_of(core_names: Iterable[str]) -> Callable[[str], bool]:
+        def __is_one_of(dirname: str) -> bool:
+            normed = dirname.strip().lower()
+            for core_name in core_names:
+                if normed.startswith(core_name):
+                    return True
+            return False
+
+        return __is_one_of
+
+    # disjunctive to conjunctive terms is weird at first but makes sense in terms of human legibility
+    name_to_predicate = {
+        "lmr": is_one_of(("lmr",)),
+        "inpatient_and_progress": is_one_of(("inpatient", "progress")),
+    }
+    synthetic_category_to_notes = merge_by_named_predicates(
+        dir_to_valid_mrn_and_date_notes, name_to_predicate
+    )
 
 
 def main():
@@ -291,5 +232,6 @@ def main():
     )
 
 
+# RPT_TEXT
 if __name__ == "__main__":
     main()
